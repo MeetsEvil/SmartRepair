5c663b05d0d9eceb308ce2c32d95324fb89729d1
Orlando Jair <orlandojgarciap@gmail.com>
2025-11-02 15:27:38 -0600

    Cambio index

--- modules/main/index.php
+++ modules/main/index.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['usuarioingresando'])) {
 
 <head>
     <meta charset="UTF-8">
-    <title>Login | SmartRepair</title>
+    <title>SmartRepair</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="../../assets/css/style.css">
