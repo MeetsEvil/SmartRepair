9cf46fbbd9bdd2e7c60b553c76a42566cd49e76b
YahirNBE <yahir.nicolasblanco12@gmail.com>
2025-11-05 13:45:09 -0600

    Agregué en usuarios la barra lateral y el ingreso con la base de datos, falta la barra superior y tabla, esta en 2 css uno para el sidebar.css y otro para el usuarios.css (para más orden que tenerlo todo en un solo css)

---
+++ modules/usuarios/index_usuarios.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+if (!isset($_SESSION['usuarioingresando'])) {
+    header("Location: ../main/index.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SmartRepair</title>
+    <link rel="stylesheet" href="../../assets/css/sidebar.css"><!--  Barra lateral de submenus -->
+    <link rel="stylesheet" href="../../assets/css/usuarios.css"><!-- Estilo general del submenu de usuarios -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
+
+<body>
+    <?php
+    // Obtiene el nombre del archivo de la URL
+    $currentPage = basename($_SERVER['REQUEST_URI']);
+    ?>
+    <div class="container">
+        <div class="navigation">
+            <ul>
+                <li class="logo">
+                    <img src="../../assets/images/logo_mattel.png" alt="logo">
+                </li>
+                <li class="<?php echo ($currentPage == 'dashboard.php') ? 'active' : ''; ?>">
+                    <a href="../main/dashboard.php" data-tooltip="Inicio">
+                        <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
+                        <span class="title">Inicio</span>
+                    </a>
+                </li>
+                <?php
+                // usuarios
+                $usuariosPages = ['index_usuarios.php', 'crear_usuarios.php', 'editar_usuarios.php', 'ver_usuarios.php'];
+                ?>
+                <li class="<?php echo in_array($currentPage, $usuariosPages) ? 'active' : ''; ?>">
+                    <a href="../usuarios/index_usuarios.php" data-tooltip="Usuario">
+                        <span class="icon"><ion-icon name="people-outline"></ion-icon></span>
+                        <span class="title">Usuarios</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" onclick="showLogoutModal()" data-tooltip="Cerrar Sesión">
+                        <span class="icon">
+                            <ion-icon name="log-out-outline"></ion-icon>
+                        </span>
+                        <span class="title">Cerrar Sesión</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+
+    <!-- Agregar barra superior -->
+
+
+    <!-- Agregar info de tabla -->
+
+
+</body>
+
+</html>
