a61e35ff418099493f026ed0f3f89a21fe718b9b
YahirNBE <yahir.nicolasblanco12@gmail.com>
2025-11-06 13:08:02 -0600

    Agregué FontAwesome

--- modules/main/dashboard.php
+++ modules/main/dashboard.php
@@ -100,6 +100,9 @@ if (!isset($_SESSION['usuarioingresando'])) {
     </div>
 
 
+    <script src="../../assets/js/main.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
